f3e73ce01d04de252c216169fb3de23446fefe10
renaud.vancoillie <renaud.vancoillie@student.howest.be>
2021-05-12 16:39:40 +0200

    Make Chat details a Feature Component

--- src/app/components/chats/details/chat.component.ts
+++ src/app/components/chats/details/chat.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
+import { Chat } from 'src/app/interfaces/chats/chat';
 import { ChatWithMessages } from 'src/app/interfaces/chats/chatWithMessages';
 
 @Component({
@@ -9,40 +10,7 @@ import { ChatWithMessages } from 'src/app/interfaces/chats/chatWithMessages';
 })
 export class ChatComponent implements OnInit {
 
-  chat: ChatWithMessages = {
-    "id": 8,
-    "guid": "07E52FF0-A2A0-4B91-85AF-77568FCA1C0C",
-    "name": "Muziek",
-    "messages": [
-        {
-            "id": 3,
-            "chatId": 8,
-            "sender": "Thomas",
-            "text": "Carry On Wayward Son",
-            "createdAt": "2021-05-12T16:20:49.75",
-            "updatedAt": null,
-            "deletedAt": null
-        },
-        {
-            "id": 4,
-            "chatId": 8,
-            "sender": "Thomas",
-            "text": "The Trooper",
-            "createdAt": "2021-05-12T16:21:05.45",
-            "updatedAt": null,
-            "deletedAt": null
-        },
-        {
-            "id": 5,
-            "chatId": 8,
-            "sender": "Jonathan",
-            "text": "Rockin' in the Free World",
-            "createdAt": "2021-05-12T16:21:38.83",
-            "updatedAt": null,
-            "deletedAt": null
-        }
-    ]
-  };
+  @Input() chat?: Chat;
 
   constructor() { }
 
