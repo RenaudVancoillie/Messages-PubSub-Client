37221e48e74bee7841653641bb1e7b484c2b7af1
renaud.vancoillie <renaud.vancoillie@student.howest.be>
2021-05-12 14:35:39 +0200

    Add Chats Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ChatsComponent } from './components/chats/chats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/chats/chats.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chats',
+  templateUrl: './chats.component.html',
+  styleUrls: ['./chats.component.css']
+})
+export class ChatsComponent implements OnInit {
+
+  chat = 'Algemeen';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
