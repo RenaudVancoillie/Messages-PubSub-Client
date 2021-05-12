32f96249a211e4b0c9d0b47c5661671816108a6d
renaud.vancoillie <renaud.vancoillie@student.howest.be>
2021-05-12 15:20:24 +0200

    Add mock-chats

---
+++ src/app/data/mock-chats.ts
@@ -0,0 +1,54 @@
+import { Chat } from '../interfaces/chat';
+
+export const CHATS: Chat[] = [
+    {
+        "id": 1,
+        "guid": "7F08D5CB-C6DF-4A24-B7E1-CB1E9C0CB320",
+        "name": "Algemeen"
+    },
+    {
+        "id": 10,
+        "guid": "D32FD024-9EEA-4521-A53F-85A597016F81",
+        "name": "Andere"
+    },
+    {
+        "id": 9,
+        "guid": "93904EC3-D0C5-4ED3-ABCD-7C512A84ABB6",
+        "name": "Beheerders"
+    },
+    {
+        "id": 5,
+        "guid": "E40933D8-24AB-4DF2-A97E-7E389558F2F0",
+        "name": "Boeken"
+    },
+    {
+        "id": 6,
+        "guid": "A7CB5DD8-EE7A-4FB5-8A39-B57390D11D66",
+        "name": "Films"
+    },
+    {
+        "id": 7,
+        "guid": "CA78889F-41A0-484E-89DB-994684EF2385",
+        "name": "Hardware"
+    },
+    {
+        "id": 8,
+        "guid": "07E52FF0-A2A0-4B91-85AF-77568FCA1C0C",
+        "name": "Muziek"
+    },
+    {
+        "id": 2,
+        "guid": "20154F99-DD2F-4517-B077-F1A60405E8BE",
+        "name": "Quotes"
+    },
+    {
+        "id": 4,
+        "guid": "C62D0F89-EE3E-40B4-84EA-87C6EA66B61F",
+        "name": "Regels"
+    },
+    {
+        "id": 3,
+        "guid": "2E06D2B1-F2CE-42D2-A1C1-BC6B0D7FDAF7",
+        "name": "Updates"
+    }
+]
