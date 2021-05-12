ebb60f27ed67f0f655ba769b465274cc39aa512f
renaud.vancoillie <renaud.vancoillie@student.howest.be>
2021-05-12 17:10:48 +0200

    Get data from server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ChatsComponent } from './components/chats/list/chats.component';
@@ -14,7 +15,8 @@ import { ChatComponent } from './components/chats/details/chat.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,20 +1,33 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 import { Chat } from 'src/app/interfaces/chats/chat';
 import { ChatWithMessages } from 'src/app/interfaces/chats/chatWithMessages';
-import { CHATS } from 'src/app/data/mock-chats';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
 
-  constructor() { }
+  private url: string = 'https://localhost:44332'
+
+  constructor(private http: HttpClient) { }
 
   getChats(): Observable<Chat[]> {
-    const chats = of(CHATS);
-    return chats;
+    const endpoint = `${this.url}/api/chats`;
+    return this.http.get<Chat[]>(endpoint).pipe(
+      catchError(this.handleError<Chat[]>(`getChats`, []))
+    )
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      console.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    }
   }
 
 }
